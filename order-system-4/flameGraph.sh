ba3c7f6b8e2bb909ba0816cef9f35a98728dbaa6
heyin <1140466863@qq.com>
2018-01-04 19:10:12 +0800

    优化项目结构

--- order-system-4/flameGraph.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-/home/gjl_workspace/lh_workspace/FlameGraph-master/stackcollapse-ljp.awk < traces.txt | /home/gjl_workspace/lh_workspace/FlameGraph-master/flamegraph.pl > traces.svg
--- order-system-5/rmidx.sh
+++
@@ -1,6 +0,0 @@
-rm diskk1/*.idx
-rm diskk2/*.idx
-rm diskk3/*.idx
-rm diskk1/*.dat
-rm diskk2/*.dat
-rm diskk3/*.dat
